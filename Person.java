868dc67e37c1f0fff0a34ee43add48d88fd6612c
Jae1994 <63680414+Jae1994@users.noreply.github.com>
2020-04-18 11:50:10 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,42 +1,53 @@
+//Vinh Nguyen, Jae Cho, Alex Larsen
+//Team Project 1
+
+//This class creates fields for a person's name and lineage (mom, dad, and children).
+//It contains a Person constructor, as well as getters and setters for the fields
+
 import java.util.*;
+
 public class Person {
+   
 	//Fields
-	private String name;
-	private String mom;
-	private String dad;
-	private ArrayList<String> children;
+   private String name;
+   private String mom;
+   private String dad;
+   private ArrayList<String> children;
+   
 	//constructor
-	public Person(String name) {
-		this.name = name;
-		children = new ArrayList<String>();
-	}
+   public Person(String name) {
+      this.name = name;
+      children = new ArrayList<String>();
+   }
+   
 	//getters, setters
-	public String getName() {
-		return name;
-	}
-	public String getMom() {
-		return mom;
-	}
+   public String getName() {
+      return name;
+   }
+   
+   public String getMom() {
+      return mom;
+   }
 
-	public void setMom(String mom) {
-		this.mom = mom;
-	}
+   public void setMom(String mom) {
+      this.mom = mom;
+   }
 
-	public String getDad() {
-		return dad;
-	}
+   public String getDad() {
+      return dad;
+   }
 
-	public void setDad(String dad) {
-		this.dad = dad;
-	}
-	//I made Children a ArrayList since there could be more than one.
-	public ArrayList<String> getChildren() {
-		return children;
-	}
+   public void setDad(String dad) {
+      this.dad = dad;
+   }
+   
+	//I made Children an ArrayList since there could be more than one.
+   public ArrayList<String> getChildren() {
+      return children;
+   }
 
-	public void setChildren(String child) {
-		children.add(child);
-		
-	}
+   public void setChildren(String child) {
+      children.add(child);
+   }
 	
 }
